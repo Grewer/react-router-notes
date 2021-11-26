f399e45fc507beeb2c2114420a6fb52f86768102
grewer <grewer@grewer.cn>
2021-11-26 23:50:25 +0800

    initial

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import {useMemo} from "react";
+
+
+function App(props) {
+
+    console.log('App', props, fn)
+
+    return (
+        <div className="App">
+
+        </div>
+    );
+}
+
+
+export default App;
