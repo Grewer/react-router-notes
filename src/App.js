a0b677c79b6a46dfaf2455c9464f45292ee6f6c0
grewer <grewer@grewer.cn>
2021-12-03 23:58:30 +0800

    feat: 添加依赖

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
 import './App.css';
 import {useMemo} from "react";
-
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 function App(props) {
 
     console.log('App', props, fn)
 
     return (
-        <div className="App">
-
-        </div>
+        <Router>
+            <Switch>
+                <Route path="/about">
+                    <About/>
+                </Route>
+                <Route path="/users">
+                    <Users/>
+                </Route>
+                <Route path="/">
+                    <Home/>
+                </Route>
+            </Switch>
+        </Router>
     );
+}
+
+
+function Home() {
+    return <h2>Home</h2>;
+}
+
+function About() {
+    return <h2>About</h2>;
+}
+
+function Users() {
+    return <h2>Users</h2>;
 }
 
 
